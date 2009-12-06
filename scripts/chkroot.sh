630104829cf62587e2df674055aa16f32789873c
Bradley Walden <linux@equk.co.uk>
2009-12-06 17:40:42 +0000

    rootkit check cron added

---
+++ scripts/chkroot.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Rootkit Check
+# ver 1.0
+# last edit: 06-12-2009
+# (dd-mm-yyyy)
+# equk / B.Walden
+#
+
+# Colors
+blue="\033[1;34m"
+green="\033[1;32m"
+red="\033[1;31m"
+bold="\033[1;37m"
+reset="\033[0m"
+
+# Check for root
+if [ $(whoami) != "root" ]; then
+    echo -e $red"error:$reset you cannot perform this operation unless you are root."
+    exit 1
+fi
+
+# log location
+log='/home/bradley/rkhunter.log'
+
+# clear log
+echo "==[ `date +%d-%m-%Y` ]==" > "$log"
+
+# execute scan
+echo -e "$blue:: Starting rkhunter check $reset"
+(/usr/bin/rkhunter --versioncheck --nocolors
+/usr/bin/rkhunter --update --nocolors
+/usr/bin/rkhunter --cronjob --nocolors --report-warnings-only) &>> "$log"
+echo -e "$green:: rkhunter scan complete please see log located $log $reset"
+
+# mail the log
+su -c "echo see attached | mutt -s \"Root Kit Check\" -a \"$log\" -- user@gmail.com" username
+echo -e "$blue:: mailing rkhunter log to administrator $reset"
