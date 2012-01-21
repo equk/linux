c6edbdb7f496f5ab649a6358bd0e28e6258573e6
Bradley Walden <linux@equk.co.uk>
2012-01-21 18:46:07 +0000

    linux config changes

--- configs/openbox/autostart.sh
+++ configs/openbox/autostart.sh
@@ -9,17 +9,23 @@ thunar --daemon &
 # little numlock tool
 numlockx &
 # parcellite clipboard tool
-parcellite &
+parcellite -n &
+#kupfer launcher
+kupfer  --no-splash &
+
 # set dual head settings
-xrandr --auto --output DVI-1 --left-of DVI-0
+#xrandr --auto --output DVI-1 --left-of DVI-0
+#xrandr --auto --output CRT1 --left-of DFP2
 # set composite settings
-xcompmgr -cfF -r7 -o.65 -l-10 -t-8 -D7 &
+#xcompmgr -cfF -r7 -o.65 -l-10 -t-8 -D7 &
 # fix mouse acceleration
 sleep 2 && xset m 1 1
 # tint2 panel
 sleep 2 && tint2 &
 # cairo-dock
-sleep 2 && cairo-dock &
+#sleep 2 && cairo-dock &
+# skippy-xd
+#sleep 2 && skippy-xd &
 # set desktop background
 nitrogen --restore &
 # start conky
