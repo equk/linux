e9605dc55619c313e05c7271943af4c26e4137fe
Bradley Walden <equk.co.uk@gmail.com>
2013-06-04 15:20:51 +0100

    few config updates

--- configs/openbox/autostart.sh
+++ configs/openbox/autostart.sh
@@ -11,33 +11,33 @@ numlockx &
 # parcellite clipboard tool
 parcellite -n &
 #kupfer launcher
-kupfer  --no-splash &
+kupfer --no-splash &
 
-# set dual head settings
+# set dual head settings (these are for the opensource amd/ati drivers)
 #xrandr --auto --output DVI-1 --left-of DVI-0
 #xrandr --auto --output CRT1 --left-of DFP2
 # set composite settings
 #xcompmgr -cfF -r7 -o.65 -l-10 -t-8 -D7 &
 # fix mouse acceleration
-sleep 2 && xset m 1 1
+sleep 1 && xset m 1 1
 # tint2 panel
-sleep 2 && tint2 &
+sleep 1 && tint2 &
 # set desktop background
 nitrogen --restore &
 # start conky
-sleep 3 && conky -q -c ~/.conkystats &
+sleep 2 && conky -q -c ~/.conkystats &
 # start conky cpu clock
-sleep 3 && conky -q -c ~/.conkycpu &
+sleep 2 && conky -q -c ~/.conkycpu &
 # set keyboard map as xorg.conf dont work
 setxkbmap -layout 'gb' -model 'pc105'
 # set DE to XFCE to fix thunar
 export DE=xfce
-# Start NetworkManager Applet
-sleep 3 && /usr/bin/nm-applet --sm-disable &
+# remove annoying WINE open with items
+rm -fr ~/.local/share/applications/wine-extension* &
 # sync chrome cache to ramdisk
 sleep 4 && $HOME/scripts/ramdisk_chrome.sh &
 # setup wacom devices to main monitor to fix scaling
 # used for dual monitor setup
-xsetwacom --set 12 MapToOutput "DFP2"
-xsetwacom --set 13 MapToOutput "DFP2"
-xsetwacom --set 14 MapToOutput "DFP2"
+xsetwacom --set 12 MapToOutput "DFP3"
+xsetwacom --set 13 MapToOutput "DFP3"
+xsetwacom --set 14 MapToOutput "DFP3"
