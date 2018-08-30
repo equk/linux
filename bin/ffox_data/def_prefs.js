775080158424dc181cf7417ac6a98bae04b6a8f1
equk <equk@users.noreply.github.com>
2018-08-30 18:33:04 +0100

    added new firefox tweaks to def_prefs.js (mainly related to possible fingerprinting)

--- bin/ffox_data/def_prefs.js
+++ bin/ffox_data/def_prefs.js
@@ -1,8 +1,10 @@
-# Mozilla User Preferences
+// Mozilla User Preferences
 
 /* Default Profile Preferences
  * ---------------------------
- * 
+ *
+ * https://github.com/equk/linux/blob/master/bin/ffox_data/def_prefs.js
+ *
  * Main changes:
  *                  + Use RAM for cache (disable disk caching)
  *                  + PDFJS disabled
@@ -14,15 +16,23 @@
  *                  + Disable Google Safebrowsing
  *                  + Disable Pocket
  *                  + Enable OMTC to fix tearing
+ *                  + Disable network (fingerprinting)
+ *                  + Disable WebRTC (fingerprinting)
+ *                  + Disable beacon (analytics)
+ *                  + Disable clipboard event detection
+ *                  + Disable matchos for locale
+ *                  + Disable Mozilla telemetry
  */
 
 user_pref("browser.cache.disk.capacity", 358400);
 user_pref("browser.cache.disk.enable", false);
 user_pref("network.cookie.cookieBehavior", 1);
+user_pref("privacy.firstparty.isolate",true);
+user_pref("network.cookie.thirdparty.sessionOnly",true);
 user_pref("pdfjs.disabled", true);
 user_pref("browser.cache.memory.enable", true);
 user_pref("browser.cache.memory.max_entry_size", -1);
-user_pref("signon.rememberSignons", false); 
+user_pref("signon.rememberSignons", false);
 user_pref("network.prefetch-next", false);
 user_pref("browser.search.suggest.enabled", false);
 user_pref("geo.enabled", false);
@@ -32,3 +42,12 @@ user_pref("browser.safebrowsing.downloads.enabled", false);
 user_pref("browser.safebrowsing.downloads.remote.enabled", false);
 user_pref("browser.pocket.enabled", false);
 user_pref("layers.acceleration.force-enabled", true);
+user_pref("dom.network.enabled",false);
+user_pref("media.peerconnection.enabled",false);
+user_pref("media.navigator.enabled",false);
+user_pref("beacon.enabled",false);
+user_pref("dom.event.clipboardevents.enabled",false);
+user_pref("intl.locale.matchOS",false);
+user_pref("toolkit.telemetry.enabled",false);
+user_pref("toolkit.telemetry.unified",false);
+user_pref("network.http.referer.spoofSource",true);
